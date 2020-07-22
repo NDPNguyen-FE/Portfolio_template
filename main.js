391fc5a60a28b915971434c2f0e5e3adbc3953c4
nguyendotnet <ndpn.dev@gmail.com>
2020-07-23 02:20:50 +0700

    new code

---
+++ main.js
@@ -0,0 +1,6 @@
+function toogle() {
+    var sec = document.getElementById('sec');
+    var nav = document.getElementById('navigation');
+    sec.classList.toggle('active');
+    nav.classList.toggle('active');
+}
